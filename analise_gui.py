de25519cd27fae4c12bcd0de99567cd0b45b7422
anacmamede <anacamilamamede@gmail.com>
2023-04-26 20:01:08 -0300

    first commit

---
+++ analise_gui.py
@@ -0,0 +1,130 @@
+import os
+import pandas as pd
+# import numpy as np
+import plotly.express as px
+# from datetime import datetime 
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+st.set_page_config(page_title='GUI - Análise', layout='wide', page_icon='📉')
+st.title("Análise de medições dos transformadores - AL02059122")
+st.markdown('Desenvolvida por Ana Camila Mamede')
+
+tick_font_size = 20 
+axis_font_size = 22
+legend_font_size = 18
+d_tick = 2
+
+def file_selector(folder_path): 
+    st.sidebar.markdown('### Escolha do arquivo')
+    with st.sidebar:
+        all_files = os.listdir(folder_path)
+        filenames = list()
+        i = 0
+        while i < len(all_files):
+            if "csv" in (all_files[i]):
+                filenames.append(all_files[i])
+            i += 1
+
+        # selected_filename = st.selectbox('Selecione um arquivo referência:', filenames, disabled=False)
+        selected_filename = st.radio( 'Selecione um arquivo:',options = filenames, disabled=False)
+        file = os.path.join(folder_path, selected_filename)
+
+    return file,selected_filename
+
+def data_preparation(df, Vbase, Sn):
+    # Data Preparation
+    df['Registro'] = pd.to_datetime(df['Registro'],format='%d/%m/%Y %H:%M')
+    df['data'] = df['Registro'].apply(lambda x: x.date())
+    df['dia'] = df['Registro'].apply(lambda x: x.day)
+    df['horario'] = df['Registro'].apply(lambda x: x.time())
+    df['hora'] = df['Registro'].apply(lambda x: x.hour)
+
+    df_max = df[['Van [V]', 'Vbn [V]', 'Vcn [V]','S [VA]','dia','hora']].groupby(['dia','hora']).max().reset_index().rename(columns={'Van [V]':'Van_max [pu]', 'Vbn [V]':'Vbn_max [pu]', 'Vcn [V]':'Vcn_max [pu]','S [VA]':'S_max [kVA]'})
+    df_max[['Van_max [pu]', 'Vbn_max [pu]', 'Vcn_max [pu]']] = df_max[['Van_max [pu]', 'Vbn_max [pu]', 'Vcn_max [pu]']]/Vbase
+    df_max['S_max [kVA]'] = df_max['S_max [kVA]']/1000
+
+    df_min = df[['Van [V]', 'Vbn [V]', 'Vcn [V]','S [VA]','dia','hora']].groupby(['dia','hora']).min().reset_index().rename(columns={'Van [V]':'Van_min [pu]', 'Vbn [V]':'Vbn_min [pu]', 'Vcn [V]':'Vcn_min [pu]','S [VA]':'S_min [kVA]'})
+    df_min[['Van_min [pu]', 'Vbn_min [pu]', 'Vcn_min [pu]']] = df_min[['Van_min [pu]', 'Vbn_min [pu]', 'Vcn_min [pu]']]/Vbase
+    df_min['S_min [kVA]'] = df_min['S_min [kVA]']/1000
+
+    df_final = pd.merge(df_max,df_min, on=['dia','hora'],how='inner')
+    df_final['Carregamento'] = df_final['S_max [kVA]']/Sn
+
+    return df_final
+
+def grafico_carregamento(df_final,dia):
+    aux = df_final[df_final['dia']==dia]
+    fig = px.line(aux,x='hora', y=['Carregamento'])
+    fig.update_xaxes(dtick=d_tick,showgrid=True)
+    fig.update_layout(title='<b>Carregamento no dia {} - Trafo {}<b>'.format(dia,trafo),xaxis_title='Hora do dia', yaxis_title='Carregamento [pu]',showlegend=False,font=dict(size=20))
+    fig.update_layout(title=dict(font = dict(size=axis_font_size+2)), legend_title = dict(font = dict(size=legend_font_size+2)), 
+                        font=dict(size=18), xaxis = dict( tickfont = dict(size=tick_font_size)), yaxis = dict( tickfont = dict(size=tick_font_size)), 
+                        xaxis_title= dict( font = dict(size=axis_font_size)), yaxis_title= dict( font = dict(size=axis_font_size)),legend = dict(font = dict(size=legend_font_size)))
+
+    return fig
+
+def grafico_tensao_max(df_final,dia):
+    aux = df_final[df_final['dia']==dia]
+    fig = px.line(aux,x='hora', y=['Van_max [pu]', 'Vbn_max [pu]', 'Vcn_max [pu]'])
+    fig.update_xaxes(dtick=d_tick,showgrid=True)
+    fig.update_layout(title='<b>Tensão máxima no dia {} - Trafo {}<b>'.format(dia,trafo),xaxis_title='Hora do dia', yaxis_title='Tensão [pu]',legend_title='',font=dict(size=20)) 
+    fig.update_layout(title=dict(font = dict(size=axis_font_size+2)), legend_title = dict(font = dict(size=legend_font_size+2)), 
+                        font=dict(size=18), xaxis = dict( tickfont = dict(size=tick_font_size)), yaxis = dict( tickfont = dict(size=tick_font_size)), 
+                        xaxis_title= dict( font = dict(size=axis_font_size)), yaxis_title= dict( font = dict(size=axis_font_size)),legend = dict(font = dict(size=legend_font_size)))
+    return fig
+
+def grafico_tensao_min(df_final,dia):
+    aux = df_final[df_final['dia']==dia]
+    fig = px.line(aux,x='hora', y=['Van_min [pu]', 'Vbn_min [pu]', 'Vcn_min [pu]'])
+    fig.update_xaxes(dtick=d_tick,showgrid=True)
+    fig.update_layout(title='<b>Tensão mínima no dia {} - Trafo {}<b>'.format(dia,trafo),xaxis_title='Hora do dia', yaxis_title='Tensão [pu]',legend_title='',font=dict(size=20))    
+    fig.update_layout(title=dict(font = dict(size=axis_font_size+2)), legend_title = dict(font = dict(size=legend_font_size+2)), 
+                        font=dict(size=18), xaxis = dict( tickfont = dict(size=tick_font_size)), yaxis = dict( tickfont = dict(size=tick_font_size)), 
+                        xaxis_title= dict( font = dict(size=axis_font_size)), yaxis_title= dict( font = dict(size=axis_font_size)),legend = dict(font = dict(size=legend_font_size)))
+
+    return fig
+
+
+if __name__ == '__main__':
+    folder_path = "datasets/"
+    file,selected_filename = file_selector(folder_path)
+    trafo = selected_filename.replace('.csv','')
+
+    st.sidebar.markdown('### Valores Base:')
+    Sn = st.sidebar.number_input('S (kVA)',value=150.0,step=0.5)
+    Vbase = st.sidebar.number_input('Tensão F-N (V)',value=220)
+
+    df = pd.read_csv(file)
+    df_final = data_preparation(df, Vbase, Sn)
+
+    selected = option_menu( menu_title=None, 
+                       options=['Visualização dos dados', 'Gráficos'],
+                       icons = ['table', 'bar-chart-fill'],
+                       orientation='horizontal')
+
+
+    if selected=='Visualização dos dados':
+        st.markdown('## Resumo:')  
+        resumo = df[['Van [V]', 'Vbn [V]', 'Vcn [V]','S [VA]','Registro']].agg([min, max]).T      
+        st.dataframe(resumo, width=400)      
+
+        st.markdown('## Dados:')
+        st.dataframe(df_final.style.highlight_min(color = 'yellow', subset=['Van_max [pu]', 'Vbn_max [pu]', 'Vcn_max [pu]',
+       'S_max [kVA]', 'Van_min [pu]', 'Vbn_min [pu]', 'Vcn_min [pu]','S_min [kVA]', 'Carregamento']).highlight_max(color = 'red', subset=['Van_max [pu]', 'Vbn_max [pu]', 'Vcn_max [pu]',
+       'S_max [kVA]', 'Van_min [pu]', 'Vbn_min [pu]', 'Vcn_min [pu]','S_min [kVA]', 'Carregamento']),height=500)
+    
+    if selected=='Gráficos':
+        st.markdown('## Gráficos 24h')
+        dia = st.number_input('Escolha o dia', value=df_final['dia'][0])
+
+        # Carregamento
+        fig_c = grafico_carregamento(df_final,dia)
+        st.plotly_chart(fig_c, use_container_width=True)
+
+        # Tensão
+        fig_v1 = grafico_tensao_max(df_final,dia)
+        st.plotly_chart(fig_v1, use_container_width=True)
+        fig_v2 = grafico_tensao_min(df_final,dia)
+        st.plotly_chart(fig_v2, use_container_width=True)
+
